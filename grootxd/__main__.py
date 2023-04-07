d8e99ead68691f644196c5e455000125dcb8acf6
ɢʀᴏᴏᴛ xᴅ <128709493+9505GrootXD@users.noreply.github.com>
2023-04-07 16:14:55 +0530

    Create __main__.py

---
+++ grootxd/__main__.py
@@ -0,0 +1,11 @@
+# GROOT NETWORK
+# G-Network Projects
+# Copyright (C) 2023 By @Groot_Network
+# Copy Rights @RJbr0 , @MyNameIsGROOT
+# Don't Any Value In This Repo If You Edit Your Github Will Get Banned 😌
+
+from pyrogram import idle
+from . import bot
+bot.start()
+idle()
+bot.stop()
